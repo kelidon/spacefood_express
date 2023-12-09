186ae2b6927e8faccb74d43ad0ea7412c9f23d48
Nikita Boyashko <nikita.boyashko@mobisystems.com>
2023-12-09 23:30:12 +0400

    map done
    has problems with camera (camera can show world out of bounds)

--- lib/flame_layer/spacefood_game.dart
+++ lib/flame_layer/spacefood_game.dart
@@ -2,21 +2,25 @@ import 'dart:async';
 
 import 'package:flame/components.dart';
 import 'package:flame/events.dart';
+import 'package:flame/experimental.dart';
 import 'package:flame/game.dart';
 import 'package:flame_bloc/flame_bloc.dart';
+import 'package:flame_tiled/flame_tiled.dart';
 import 'package:flutter/material.dart';
 
 import '../actors/player.dart';
 import '../blocs/game_stats/game_stats_bloc.dart';
 import '../blocs/inventory/inventory_bloc.dart';
 
-class GameStatsController extends Component with HasGameReference<SpaceFoodGame> {
+class GameStatsController extends Component
+    with HasGameReference<SpaceFoodGame> {
   @override
   Future<void>? onLoad() async {
     add(
       FlameBlocListener<GameStatsBloc, GameStatsState>(
         listenWhen: (previousState, newState) {
-          return previousState.status != newState.status && newState.status == GameStatus.initial;
+          return previousState.status != newState.status &&
+              newState.status == GameStatus.initial;
         },
         onNewState: (state) {
           //game.removeWhere((element) => element is EnemyComponent);
@@ -29,15 +33,30 @@ class GameStatsController extends Component with HasGameReference<SpaceFoodGame>
 class SpaceFoodGame extends FlameGame
     with PanDetector, HasCollisionDetection, HasKeyboardHandlerComponents {
   late PlayerComponent player;
+  late TiledComponent mapComponent;
 
   final GameStatsBloc statsBloc;
   final InventoryBloc inventoryBloc;
 
-  SpaceFoodGame({required this.inventoryBloc, required this.statsBloc});
+  SpaceFoodGame({
+    required this.inventoryBloc,
+    required this.statsBloc,
+  });
 
   @override
   Future<void> onLoad() async {
-    await add(
+    mapComponent = await TiledComponent.load('testmap.tmx', Vector2.all(32));
+    print(camera.viewport.size);
+    final cameraVisibleArea =
+        Rectangle.fromRect(Rect.fromLTRB(0, 0, 3200, 3200));
+    camera.setBounds(cameraVisibleArea, considerViewport: false);
+    player = PlayerComponent();
+
+    player.anchor = Anchor.center;
+
+    camera.follow(player);
+
+    await world.add(
       FlameMultiBlocProvider(
         providers: [
           FlameBlocProvider<GameStatsBloc, GameStatsState>.value(
@@ -48,11 +67,20 @@ class SpaceFoodGame extends FlameGame
           ),
         ],
         children: [
-          player = PlayerComponent(),
-          PlayerController(),
-          GameStatsController(),
+          mapComponent,
+          player,
         ],
       ),
+    );
+    await add(world);
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    player.position = Vector2(
+      player.position.x.clamp(0, mapComponent.size.x),
+      player.position.y.clamp(0, mapComponent.size.y),
     );
   }
 
