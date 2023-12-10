a7e06832b36af0d121dfd9d0d252d0df502f8b24
Viktar Shchors <v.shchors@gmail.com>
2023-12-11 00:02:35 +0400

    ui

--- lib/flutter_layer/compass_widget.dart
+++ lib/flutter_layer/compass_widget.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:spacefood_express/blocs/compass/compass_cubit.dart';
@@ -9,8 +11,8 @@ class CompassWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     final compassCubit = context.watch<CompassCubit>();
     return Transform.rotate(
-      angle: compassCubit.state.angle,
-      child: Container(color: Colors.red, child: Text("i")),
+      angle: compassCubit.state.angle + pi/2,
+      child: Image.asset('assets/images/compass.png', width: 50,height: 50,),
     );
   }
 }
